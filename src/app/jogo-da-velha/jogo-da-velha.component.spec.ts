543dcba6aa24424ddc3f3102ccd50d064ae85891
= <=>
2021-11-03 14:01:16 -0300

    atualizados os testes

--- src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+import { JogoDaVelhaComponent, JogoDaVelhaService } from '../jogo-da-velha';
 
 describe('JogoDaVelhaComponent', () => {
   let component: JogoDaVelhaComponent;
@@ -8,7 +8,8 @@ describe('JogoDaVelhaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ JogoDaVelhaComponent ]
+      declarations: [ JogoDaVelhaComponent ],
+      providers: [ JogoDaVelhaService ]
     })
     .compileComponents();
   });
